0a32553e2f4a38497cf7943f9c5525de9d8183ba
dimshik100 <dimshik100@gmail.com>
2018-09-14 22:48:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# The Movie Database Ionic App
+This is a responsive example app developed using Ionic 3 that calls the movie database API.
+It allow user to search for any movie.
+
+## Installation 
+If you would like to download the code and try it for yourself:
+
+*please, notive that you might have ionic installed already
+
+1. Clone the repo: https://github.com/dimshik100/did-dimshik-watched-that-movie.git
+2. cd ionic-tmdb
+3. Install packages: npm install
+4. Build project and launch: ionic serve
+5. Open your browser at: http://localhost:8100
+
+## Installation Android
+* ionic run cordova android
+
+# Screenshot
+
+
+## Author
+[Dima Vishnevetsky](http://www.dimshik.com/)
+
+## License
+[MIT](https://github.com/dimshik100/did-dimshik-watched-that-movie/blob/master/License)
+
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { MoviesModule } from './../pages/movies/movies.module';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    MoviesModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+  ]
+})
+export class AppModule {}
